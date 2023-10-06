ddbd3904d8e562e5991988f7111b3109b07524eb
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-06 11:55:06 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,26 @@
+var a=[];
+var n=parseInt(prompt("Enter the size of the array"));
+
+	
+for (let i=0; i<n; i++){
+	a[i]=parseInt(prompt("Enter the" + (i+1) +" value"));
+}
+
+document.write("Given Array = "+a+"<br><br>");
+
+var b=[];
+var temp
+
+for (let i=0; i<a.length;  i++){
+	for (let j=i+1; j<a.length; j++){
+		if(a[i]>a[j]){
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+	}
+		b.push(a[i]);
+}
+var c=b[0];
+var d=b[1];
+document.write("the first and second smallest number in the array is"+c+ ","+d)
